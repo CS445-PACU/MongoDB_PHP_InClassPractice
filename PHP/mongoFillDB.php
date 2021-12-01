de60b1e0ab69c57014aea138d642eeeae6465693
chadd williams <chaddcw+github@gmail.com>
2021-11-30 20:29:01 -0800

    add steps to fillDB

---
+++ PHP/mongoFillDB.php
@@ -0,0 +1,20 @@
+
+<?php
+require 'vendor/autoload.php';
+
+// 1. Connect to MongoDB
+
+// 2. Get collection
+
+// 3. Read JSON from web
+$jsonBookText = file_get_contents('https://corgis-edu.github.io/corgis/datasets/json/classics/classics.json');
+
+// 4. Convert JSON from String to Array
+
+// var_dump($jsonBookArray);
+
+// 5. Insert the entire array 
+
+// 6. Determine how many were inserted
+
+?>
