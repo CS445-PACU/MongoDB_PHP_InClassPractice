5dd253c1210d0520b7fcdcbbf91091bb0f636661
chadd williams <chaddcw+github@gmail.com>
2021-11-30 21:56:34 -0800

    add delete insert one steps

---
+++ PHP/mongoDeleteInsertOne.php
@@ -0,0 +1,25 @@
+
+<?php
+require 'vendor/autoload.php';
+
+// 1. Connect to MongoDB
+
+// 2. Get collection
+
+// 3. Delete many
+
+// 4. Read JSON from web
+$jsonBookText = file_get_contents('https://corgis-edu.github.io/corgis/datasets/json/classics/classics.json');
+
+// 5. Convert JSON from String to Array
+
+// 6. Insert one document
+
+// 7. Determine how many were inserted
+
+// 8. Query one
+
+// 9. Display one
+
+// 10. Delete many
+?>
